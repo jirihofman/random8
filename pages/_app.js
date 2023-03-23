b392ee54076f7b0ad1742ae00bded1f52fa6e647
Jiří Hofman <jiri.hofman@gmail.com>
2023-03-23 22:42:55 +0100

    upgrade steps, dates

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css'
-import '../node_modules/toastr/build/toastr.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
