e40d1d112caabb7f477be1ed31bb16b1e338ec3d
Jiří Hofman <jiri.hofman@gmail.com>
2023-09-19 21:24:00 +0200

    use turbopack, use port 3000

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		appDir: true,
-	},
-	// i18n not supported by turbopack yet.
-	// i18n: {
-	// 	locales: ['en'],
-	// 	defaultLocale: 'en',
-	// }
+	i18n: {
+		locales: ['en'],
+		defaultLocale: 'en',
+	}
 }
 
 module.exports = nextConfig;
